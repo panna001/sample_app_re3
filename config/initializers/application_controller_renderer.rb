9250a55a86c63a30dce08bfec30f60fd1bbaac5c
DaichiIshikawa <sonar.pocket.0926@gmail.com>
2021-01-20 10:18:05 +0000

    [Finish]3-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '627fcc6c32d2ce2bd1049891b9efc5410390507790c2a8720215b1aca7091b6bff493c8fa9d7078097b4e3b2cf60744e8b4e59337335c3ace034125b04287a31'
